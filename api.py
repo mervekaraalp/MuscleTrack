20bb3af776662d32ad44269dd026ac8d4b7faeb1
mervekaraalp <merveekaraalp@gmail.com>
2025-05-04 04:33:13 +0300

    Password hashing method updated

--- api.py
+++ api.py
@@ -55,7 +55,8 @@ def register_api():
     if User.query.filter_by(username=data['username']).first():
         return jsonify({'message': f"Kullanıcı adı '{data['username']}' zaten alınmış!"}), 400  # Kullanıcı adı belirtildi
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+    hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256')
+
     new_user = User(username=data['username'], password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
