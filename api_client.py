2b67e9d16db7550f3883143cd4bf97f9f5e99240
mervekaraalp <142693142+mervekaraalp@users.noreply.github.com>
2025-05-04 04:09:47 +0300

    api_client.py

--- api_client.py
+++ api_client.py
@@ -1,9 +1,7 @@
 import requests
 
-# API base URL
 API_URL = "https://muscletrack.onrender.com"
 
-# Kullanıcı girişi
 def login_user(username, password):
     response = requests.post(f"{API_URL}/login_api", json={
         "username": username,
@@ -11,12 +9,15 @@ def login_user(username, password):
     })
     return response.json()
 
-# Kullanıcı kaydı
 def register_user(username, password):
-    response = requests.post(f"{API_URL}/register_api", json={
-        "username": username,
-        "password": password
-    })
-    return response.json()
+    try:
+        response = requests.post(f"{API_URL}/register_api", json={
+            "username": username,
+            "password": password
+        })
+        return response.json()
+    except Exception as e:
+        return {"message": f"API hatası: {str(e)}"}
+
 
 
