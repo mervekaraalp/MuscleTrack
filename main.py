a2e98762318c91e6ce59dc2b0aa84dbb56a40eee
mervekaraalp <142693142+mervekaraalp@users.noreply.github.com>
2025-05-25 21:21:40 +0300

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import streamlit as st
+from pages import login, register, sensor_data
 
-# Sayfa başlığı
 st.set_page_config(page_title="MuscleTrack", page_icon="💪")
 
-# Menü
-menu = ["Ana Sayfa", "Egzersiz", "Sensör Verileri", "Ayarlar"]
+# Oturum başlatma (ilk giriş)
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+    st.session_state.username = ""
+
+# Sidebar menü
+menu = ["Giriş Yap", "Kayıt Ol"]
+if st.session_state.logged_in:
+    menu = ["Sensör Verileri", "Çıkış Yap"]
+
 choice = st.sidebar.selectbox("Navigasyon", menu)
 
-# Sayfa içeriği
-if choice == "Ana Sayfa":
-    st.write("MuscleTrack hakkında daha fazla bilgi.")
-elif choice == "Egzersiz":
-    st.write("Egzersiz sayfasına hoş geldiniz!")
+# Menü seçeneklerine göre sayfa içeriği
+if choice == "Giriş Yap":
+    login.app()
+
+elif choice == "Kayıt Ol":
+    register.app()
+
 elif choice == "Sensör Verileri":
-    st.write("Sensör verileri sayfasına hoş geldiniz!")
-elif choice == "Ayarlar":
-    st.write("Ayarlar sayfasına hoş geldiniz!")
+    sensor_data.app()
+
+elif choice == "Çıkış Yap":
+    st.session_state.logged_in = False
+    st.session_state.username = ""
+    st.success("Başarıyla çıkış yaptınız.")
